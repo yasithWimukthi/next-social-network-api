ec8e6935bc6d6af9c6ba95b935750d683ce7fd96
yasith wimukthi <it19966922@my.sliit.lk>
2021-12-05 10:40:17 +0530

    implement registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ export const register = async (req,res) => {
     const {name,email,password,secret} = req.body;
 
     if (!name) return res.status(400).send('Name is required');
-    if (!password || password.length<=6){
+    if (!password || password.length<6){
         return res.status(400).send('Password is required and must be at least 6 characters');
     }
     if (!secret) return res.status(400).send('Answer is required');
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 import express, {json} from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
+const authRoutes = require('./routes/auth');
+const bodyParser = require('body-parser');
 
 const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
+app.use(bodyParser.json());
 
+app.use(morgan('dev'));
 app.use(express.json({limit:'5mb'}));
 app.use(express.urlencoded({extended:true}));
-app.use(cors({
-    origin:['http://localhost:3000/']
-}))
+// app.use(cors({
+//     origin:['http://localhost:3000/']
+// }))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+});
+
+app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.CONNECTION_URL)
     .then(()=>{
