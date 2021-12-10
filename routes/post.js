7acf0e83eebb96285d45cb285da2f0cd7e8a112e
yasith wimukthi <it19966922@my.sliit.lk>
2021-12-10 09:50:18 +0530

    render posts

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {requireSignIn} = require("../middlewares/auth");
 import formidable from 'express-formidable';
-const {createPost,uploadImage} = require("../controllers/post");
+const {createPost,uploadImage,postsByUser} = require("../controllers/post");
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post('/create-post',requireSignIn,createPost);
 router.post('/upload-image',requireSignIn,formidable({
     maxFileSize: 5*1024*1024
 }),uploadImage);
+router.get('/user-posts',requireSignIn,postsByUser);
 
 module.exports = router;
