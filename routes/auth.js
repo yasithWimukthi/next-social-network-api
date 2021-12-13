901409a039b54387a09646663bbcc0d60d0db373
yasith wimukthi <it19966922@my.sliit.lk>
2021-12-13 21:48:15 +0530

    implement follow user

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const {register,login,currentUser,forgotPassword,profileUpdate} = require("../controllers/auth");
+const {
+    register,
+    login,
+    currentUser,
+    forgotPassword,
+    profileUpdate,
+    findPeople
+} = require("../controllers/auth");
 const {requireSignIn} = require("../middlewares/auth");
 
 const router = express.Router();
@@ -9,5 +16,6 @@ router.post('/login',login);
 router.get('/current-user',requireSignIn,currentUser);
 router.post('/forgot-password',forgotPassword);
 router.post('/update-profile',requireSignIn,profileUpdate);
+router.get('/find-people',requireSignIn,findPeople);
 
 module.exports = router;
